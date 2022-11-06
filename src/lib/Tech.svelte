b0e317ba222410b0c46c80954c066ae370de0dc5
rathlucas <lucin189@gmail.com>
2022-11-06 19:11:07 -0300

    Create Tech component

--- src/lib/Tech.svelte
+++ src/lib/Tech.svelte
@@ -2,7 +2,7 @@
   import Skills from './Tech.Skills.svelte'
 </script>
 
-<section class="px-20 lg:px-36">
+<section>
   <div class="flex flex-col justify-center items-center">
     <h2
       class="text-2xl text-blue-700 text-opacity-90 font-bold uppercase mb-4 select-none"
