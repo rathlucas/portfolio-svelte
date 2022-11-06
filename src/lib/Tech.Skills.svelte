20d3c7bb140c3ea608a08b2b2d1cc055359098f7
rathlucas <lucin189@gmail.com>
2022-11-06 19:10:55 -0300

    Create Skills component

--- src/lib/Tech.Skills.svelte
+++ src/lib/Tech.Skills.svelte
@@ -1,14 +1,17 @@
 <div class="flex w-full">
-  <ul class="flex flex-col justify-start w-full select-none">
+  <ul class="flex flex-col w-full select-none">
     <li class="mb-2">
       <img
         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
         width="40"
         height="40"
         alt="Javascript logo"
-        class="inline"
+        class="inline-flex"
       />
       Javascript (ES6+)
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(240,219,79)] text-white w-[90%]">90%</p>
+      </div>
     </li>
     <li class="mb-2">
       <img
@@ -16,9 +19,12 @@
         width="40"
         height="40"
         alt="Typescript logo"
-        class="inline"
+        class="inline-flex"
       />
       Typescript
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(0,122,204)] text-white w-[75%]">75%</p>
+      </div>
     </li>
     <li class="mb-2">
       <img
@@ -26,9 +32,12 @@
         width="40"
         height="40"
         alt="React logo"
-        class="inline"
+        class="inline-flex"
       />
       React
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(97,218,251)] text-white w-[80%]">80%</p>
+      </div>
     </li>
     <li class="mb-2">
       <img
@@ -36,9 +45,12 @@
         width="40"
         height="40"
         alt="Next Js logo"
-        class="inline"
+        class="inline-flex"
       />
       NextJS
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-black text-white w-[75%]">75%</p>
+      </div>
     </li>
     <li class="mb-2">
       <img
@@ -46,9 +58,12 @@
         width="40"
         height="40"
         alt="Node Js logo"
-        class="inline"
+        class="inline-flex"
       />
       Node
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(131,205,41)] text-white w-[90%]">90%</p>
+      </div>
     </li>
     <li class="mb-2">
       <img
@@ -56,31 +71,51 @@
         width="40"
         height="40"
         alt="Adonis Js logo"
-        class="inline"
+        class="inline-flex"
       />
       AdonisJS
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(90,69,255)] text-white w-[85%]">85%</p>
+      </div>
+    </li>
+    <li class="mb-2">
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+        width="40"
+        height="40"
+        alt="HTML logo"
+        class="inline-flex"
+      />
+      HTML Semântico
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(241,101,41)] text-white w-[100%]">100%</p>
+      </div>
+    </li>
+    <li class="mb-2">
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+        width="40"
+        height="40"
+        alt="CSS logo"
+        class="inline-flex"
+      />
+      CSS
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(51,169,220)] text-white w-[90%]">90%</p>
+      </div>
+    </li>
+    <li class="mb-2">
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+        width="40"
+        height="40"
+        alt="Tailwind CSS logo"
+        class="inline-flex"
+      />
+      TailwindCSS
+      <div class="bg-gray-400 w-full text-center">
+        <p class="bg-[rgb(56,178,172)] text-white w-[80%]">80%</p>
+      </div>
     </li>
   </ul>
-  <div
-    class="flex flex-col justify-around items-center w-full select-none text-white"
-  >
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-    <div class="bg-gray-400 w-full">
-      <p class="bg-blue-600 w-[90%]">90%</p>
-    </div>
-  </div>
 </div>
