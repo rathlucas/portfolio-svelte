a683b7687260b9a330dd201e44ff3021138aad97
rathlucas <lucin189@gmail.com>
2022-11-15 14:22:40 -0300

    Create disclosure functionality to Skills rndering

--- src/lib/Tech.svelte
+++ src/lib/Tech.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
+  import { slide, fade } from 'svelte/transition'
   import Skills from './Tech.Skills.svelte'
+  import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte'
+
+  let isDisclosureOpen = false
+  function handleOpenDisclosure() {
+    isDisclosureOpen = !isDisclosureOpen
+  }
 </script>
 
 <section>
@@ -9,6 +16,20 @@
     >
       Tecnologias
     </h2>
-    <Skills />
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      on:click={handleOpenDisclosure}
+      class="flex justify-between items-center w-full mb-8 px-2 rounded-lg transition-all ease-out hover:bg-slate-300"
+    >
+      <span class="inline-flex w-[90%] h-[0.10rem] bg-slate-500" />
+      <div class="inline-flex w-4 h-4 text-slate-800">
+        <FaAngleDown />
+      </div>
+    </div>
+    {#if isDisclosureOpen}
+      <div class="w-full" in:fade out:slide={{ duration: 500 }}>
+        <Skills />
+      </div>
+    {/if}
   </div>
 </section>
